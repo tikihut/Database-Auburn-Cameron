a69c9407f4e385e32ade3add3b02df311ace077e
tikihut <kiba1996@gmail.com>
2018-12-09 18:06:33 -0600

    Add files via upload
    Tabs Working and look better. TODO: Prevent DROP TABLE

--- index.php
+++ index.php
@@ -51,34 +51,13 @@ body {font-family: Arial;}
 </style>
 </head>
 <body>
-<h1>Welcome</h1>
-<img src="https://images-na.ssl-images-amazon.com/images/I/6101Je6svzL._SX425_.jpg" class="dab" />
+<h1>Welcome To Cameron Taylor's SQL DB Search</h1>
 <br>
-<p> Search The Database</p>
+<p><h1>Search The Database</h1></p>
 <form action="" method="post">
   <input type="text" name="firstname" id="firstname" size="40" >
   <input type="submit" name="submit" value="Submit">
 </form>	
-<div class="tab">
-  <button class="tablinks" onclick="openTab(event, 'Big Balls')">Big Balls</button>
-  <button class="tablinks" onclick="openTab(event, 'Bigger Balls')">Bigger Balls</button>
-  <button class="tablinks" onclick="openTab(event, 'My Balls')">My Balls</button>
-</div>
-?>
-<div id="Big Balls" class="tabcontent">
-  <h3>Big Balls</h3>
-  <p>These are the balls mitch likes</p>
-</div>
-
-<div id="Bigger Balls" class="tabcontent">
-  <h3>Bigger Balls</h3>
-  <p>These are the balls Harshil likes</p> 
-</div>
-
-<div id="My Balls" class="tabcontent">
-  <h3>My Balls</h3>
-  <p>These are the balls everyone likes</p>
-</div>
 
 <script>
 function openTab(evt, cityName) {
@@ -134,7 +113,7 @@ $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 } 
-echo "Connected to the damn SQL server successfully <br>";
+echo "Connected to the SQL server successfully <br>";
 /* fetch result */
 
 while ($row = mysqli_fetch_row($result))
@@ -146,71 +125,27 @@ echo $row['column_name'];
 //$num = mysqli_$affected_rows($con);
 $str = mysqli_connect_error();
 $err = mysqli_connect_errno();
-
 $str = mysqli_error($con);
 $err = mysqli_errno($con);
-//
-//Show Tables based on tables active in DB
-//
-$result2 = mysqli_query($conn, 'Show Tables');
-$tables_array = array();
-$i=1;
-echo "Testing Tables <br>";
-while($row = mysqli_fetch_assoc($result2))
-{	
-	//$tables_array[$i]=$row['Tables_in_cdt0020db'];
-	?>
-	<div class="tab">
-		<button class="tablinks" onclick="openTab(event, '<?php echo $row['Tables_in_cdt0020db']?>')"><?php echo $row['Tables_in_cdt0020db'];?></button>
-	</div>
-
-	<div id="<?php echo $row['Tables_in_cdt0020db']?>" class="tabcontent">
- 	<h3><?php echo $row['Tables_in_cdt0020db']?></h3>
-  	<p>
-		<?php 
-		//Find all columns of tab
-		$find_columns = "show columns from ";
-		$current_table = $row["Tables_in_cdt0020db"];
-		$find_columns .= $current_table;
-		$search_for_tab = mysqli_query($conn, $find_columns);
-		if (mysqli_num_rows($search_for_tab) > 0)
-		{
-			while($row = mysqli_fetch_assoc($search_for_tab))
-			{
-				$field = $row["Field"];
-				$select_from = "SELECT * FROM ";
-				$select_from .= $current_table;
-				$result_tab = mysqli_query($conn, $select_from);
-				if (mysqli_num_rows($result_tab) > 0) {
-					echo "<table><tr><th>$field</th>";
-					while($row = $result_tab->fetch_assoc()) {
-						echo "<tr><td>".$row["$field"]."</td><td>";
-					}
-					echo "</table>";
-				} 
-				else {
-					echo "0 results<br>";
-				}
-			} 
-		}
-		?>
-	</p>
-	</div>
-<?php
-	//$i++;
-}
-echo "END Testing Tables <br>";
-//
+//**********************************
 //Query Search Section
-//
+//**********************************
 if(isset($_POST['submit']))
 {
-	echo "Submited <br></br>";
+	echo "Submited Query: <br>";
 	// Take input
 	$test = $_POST['firstname'];
 	$test4="";
 	$test4 .= $test;
-	echo "Running Query: ";
+	echo $test4."<br><br>";
+	
+	//$dabcheck = "Dab";
+	/*This is just something i put in at 2am for no reason
+	if($test4=$dabcheck){
+		echo "<h1>Dab on these haters</h1><br>";
+		?><img src="https://images-na.ssl-images-amazon.com/images/I/6101Je6svzL._SX425_.jpg" class="dab" /><br><?php
+	}*/
+	echo "Checking Query: ";
 	// Strip slashes from input
 	$testStrip = stripslashes($test4);
 	//run query from input
@@ -232,9 +167,12 @@ if(isset($_POST['submit']))
 	//then find all fields of that table
 	//so that i can display correct rows
 	$results = mysqli_query($conn, $search_phrase);
-	echo "<br>";
 	if (mysqli_num_rows($results) > 0)
 	{
+		?>
+		<font size="4" color="Green">Vallid query submited: </font>
+		<?php
+		echo "<br>";
 		while($row = mysqli_fetch_assoc($results))
 		{
 			$contents = $row["Field"];
@@ -262,8 +200,69 @@ if(isset($_POST['submit']))
 		}
    	
 	}
+	else {
+		?>
+		<font size="4" color="red">Non Valid SQL Submited</font>
+		<?php
+	}
 }
-//
+// ****************
 // End Query Search
-//
+// ****************
+
+
+//*************************************
+//Show Tables based on tables active in DB
+//*************************************
+$result2 = mysqli_query($conn, 'Show Tables');
+$tables_array = array();
+$i=1;
+echo "<h1>Database Tables:</h1> <br>";
+while($row = mysqli_fetch_assoc($result2))
+{	
+	//$tables_array[$i]=$row['Tables_in_cdt0020db'];
+	?>
+	<div class="tab">
+		<button class="tablinks" onclick="openTab(event, '<?php echo $row['Tables_in_cdt0020db']?>')"><?php echo $row['Tables_in_cdt0020db'];?></button>
+	</div>
+
+	<div id="<?php echo $row['Tables_in_cdt0020db']?>" class="tabcontent">
+ 	<h3><?php echo $row['Tables_in_cdt0020db']?></h3>
+  	<p>
+		<?php 
+		//Find all columns of tab
+		$find_columns = "show columns from ";
+		$current_table = $row["Tables_in_cdt0020db"];
+		$find_columns .= $current_table;
+		$search_for_tab = mysqli_query($conn, $find_columns);
+		if (mysqli_num_rows($search_for_tab) > 0)
+		{
+			while($row = mysqli_fetch_assoc($search_for_tab))
+			{
+				$field = $row["Field"];
+				$select_from = "SELECT * FROM ";
+				$select_from .= $current_table;
+				$result_tab = mysqli_query($conn, $select_from);
+				if (mysqli_num_rows($result_tab) > 0) {
+					echo "<table><tr><nobr><th><nobr>$field</th>";
+					while($row = $result_tab->fetch_assoc()) {
+						echo "<tr><nobr><td><nobr>".$row["$field"]."</td><td>";
+					}
+					echo "</table>";
+				} 
+				else {
+					echo "0 results<br>";
+				}
+			} 
+		}
+		?>
+	</p>
+	</div>
+<?php
+	//$i++;
+}
+//**********
+//END Tables
+//**********
+
 ?>
